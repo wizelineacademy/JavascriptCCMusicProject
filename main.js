94e0323e3c6f8f10faa00f0d8a6e04fdd7316e09
Romina Espinosa <romina.espinosa@wizeline.com>
2017-08-01 16:09:04 -0500

    Add a basic function calling a high order function

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
 'use strict'
-console.log('hello world!')
+
+const files = [
+  'track1.wav',
+  'track2.wav',
+  '.cache'
+]
+
+const listTrackFiles = () => {
+  return files.filter( file => file.endsWith('.wav') )
+}
+
+console.log('tracks:')
+console.log(listTrackFiles())
