6b32a52263987181dd180f6e1db5cba7d815675d
Romina Espinosa <romina.espinosa@wizeline.com>
2017-08-07 11:02:38 -0500

    Clean comments

--- musicLibrary.js
+++ musicLibrary.js
@@ -8,19 +8,6 @@ const files = [
 
 const endsWithWav = file => file.endsWith('.wav');
 
-// ie. Using imperative programming
-/*function listTrackFiles() {
-  const wavFiles = [];
-  for(let i=0; i<files.length; i++) {
-    const file = files[i];
-    if(endsWithWav(file)) {
-      wavFiles.push(file);
-    }
-  }
-  return wavFiles;
-}*/
-
-// ie. Using a high order function
 function listTrackFiles() {
   return files.filter( endsWithWav );
 }
